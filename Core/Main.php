07bd3fc5846e712d64d5b8ee7da5f6fdc21d4dda
bobo <mbdiallo@devalto.com>
2022-09-20 09:24:43 +0000

    BG-019: Fix delete comment

--- Core/Main.php
+++ Core/Main.php
@@ -20,7 +20,7 @@ class Main {
 
 			// On redirige vers l'URL dans /
 			header('Location: ' . $uri);
-
+			exit();
 		}
 
 		// On sépare les paramètres et on les met dans le tableau $params
@@ -32,7 +32,7 @@ class Main {
 			$controller = '\\App\\Controllers\\' . ucfirst(array_shift($params)) . 'Controller';
 			if (!class_exists($controller)) {
 				$exceptionController->notFoundAction($_GET['p']);
-				return;
+				exit();
 			}
 			// On sauvegarde le 2ème paramètre dans $action si il existe, sinon index
 			$action = isset($params[0]) ? array_shift($params) : 'index';
@@ -50,6 +50,7 @@ class Main {
 						$action
 					)
 				);
+				exit();
 			}
 		} else {
 			// Ici aucun paramètre n'est défini
--- Models/CommentModel.php
+++ Models/CommentModel.php
@@ -11,14 +11,25 @@ class CommentModel extends Model {
 	/**
 	 * fonction pour afficher tout les commentaires d'un post précis
 	 * @param $idPost
-	 * @return array|false
+	 * @return Commentaire[]
 	 */
-	public function displayCommentAdmin($idPost) {
+	public function getCommentByPostId($idPost) {
 
 		$bdd = $this->getDB();
-		$requete = $bdd->prepare('SELECT*FROM commentaire WHERE id_postc = ? AND verification = "en cours"');
+		$requete = $bdd->prepare('SELECT * FROM commentaire WHERE id_postc = ? AND verification = "en cours"');
 		$requete->execute(array($idPost));
 		return $requete->fetchAll(PDO::FETCH_CLASS,Commentaire::class);
+	}
+
+	/**
+	 * @param $commentId
+	 * @return Commentaire|null
+	 */
+	public function getCommentById($commentId) {
+		$bdd = $this->getDB();
+		$requete = $bdd->prepare('SELECT * FROM commentaire WHERE id = ?');
+		$requete->execute([$commentId]);
+		return $requete->fetchAll(PDO::FETCH_CLASS,Commentaire::class)[0];
 	}
 
 	public function listCommentsOfPost($idPost) {
@@ -30,17 +41,20 @@ class CommentModel extends Model {
 
 	public function validateComment($idComment) {
 		$bdd = $this->getDB();
-		$requete = $bdd->prepare('UPDATE commentaire SET verification = "validee" WHERE id =?');
+		$requete = $bdd->prepare('UPDATE commentaire SET verification = "validee" WHERE id = ?');
 		$requete->execute($idComment);
 		$requete->fetchAll(PDO::FETCH_CLASS,Commentaire::class)[0];
-		header("Location:" . "http://localhost:8080/public?p=adminPost");
-		alert("Commentaire validé");
 	}
 
 	public function addComment($idPost, $comment, $date) {
 		$bdd = $this->getDB();
 		$requete = $bdd->prepare('INSERT INTO commentaire (id_postc,commentaire,date)  VALUES (?, ?, ?)');
 		$requete->execute(array($idPost, $comment, $date));
+	}
+
+	public function deleteComment($id) {
+		$bdd = $this->getDB();
+		$bdd->exec('DELETE FROM commentaire WHERE id = ' . $id);
 	}
 
 }
