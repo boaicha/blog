99d02f7eb83e534f5dde5768c08cb57ad6565f86
aicha <aichabouhouch.ab@gmail.com>
2023-06-27 16:24:25 +0200

    image de base de donnée

--- sqlBlogP5.sql
+++ sqlBlogP5.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `blog1`
+-- Base de données : `blog`
 --
 
 -- --------------------------------------------------------
